3525926bea82b2e0ad2b74fa9db8cb7c1d98f936
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2020-12-04 17:34:14 -0600

    project hover and link changed to child div. added gradient and toggle to show detailed description

--- app.js
+++ app.js
@@ -8,26 +8,49 @@ let sheetAsJSON = "https://spreadsheets.google.com/feeds/list/1deWdcbHJbl85QS8QR
 const render = (projectsArr) => {
     projectsArr.forEach(elem => {
         const $div = $('<div class="indivProj"></div>')
-        $div.attr('href', elem.link)
         $('div.portContainer').append($div)
 
-        const $img = $('<img class="projImg">')
-        $img.attr('src', elem.image).attr('alt', 'project preview')
-        $($div).html($img)
+        const $previewDiv = $(`<div class="previewDiv"></div>`)
+        $previewDiv.attr('href', elem.link)
+        $($div).append($previewDiv)
 
-        const $p = $(`<p class="title">${elem.title}</p>`)
-        $($div).append($p)
+            const $p = $(`<p class="title">${elem.title}</p>`)
+            $($previewDiv).append($p)
 
+            const $img = $('<img class="projImg">')
+            $img.attr('src', elem.image).attr('alt', 'project preview')
+            $($previewDiv).append($img)
+
+        //  add div for hover opacity
+            const $view = $('<div class="view"></div>')
+            $view.text("View Project")
+            $($previewDiv).append($view)
+        
         const $p2 = $(`<p class="technology">${elem.technology}</p>`)
         $($div).append($p2)
 
-        //add div for hover opacity
-        const $view = $('<div class="view"></div>')
-        $view.text("View Project")
-        $($div).append($view)
+           
 
-        $div.click(function () {
+        // const $button = $('<button></button>')
+        // $button.text("button")
+        // $($div).after($button)
+        $p2.click(function() {
+            $($p3).toggle()
+        })
+
+        const $p3 = $(`<p class="description">${elem.description}</p>`)
+        $($div).append($p3)
+
+        
+
+        $previewDiv.click(function () {
             window.location = $(this).attr('href');
+
+        
+        $p2.click(function() {
+                $($p3).toggle()
+            })
+        
 
         });
     })
@@ -41,6 +64,7 @@ $.ajax({ url: sheetAsJSON })
                 image: project.gsx$image.$t,
                 technology: project.gsx$technology.$t,
                 link: project.gsx$link.$t,
+                description: project.gsx$description.$t
             }
         })
         render(projects)
@@ -77,3 +101,6 @@ if ($(window).width() < 769) {
     }
     $a.on('click', hideMenu)
 }
+
+
+// $div.attr('href', elem.link)
